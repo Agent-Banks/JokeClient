9bca34bb948f8c658978476dabab3b4ba76d343c
CodyBanks <codydouglasbanks@gmail.com>
2020-06-02 15:54:29 -0400

    finished first step

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace JokeClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        class Joke
+        {
+            public int id { get; set; }
+            public string type { get; set; }
+            public string setup { get; set; }
+            public string punchline { get; set; }
+        }
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/programming/random");
+            var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+
+            foreach (var joke in jokes)
+            {
+                Console.WriteLine($"This is a {joke.type} joke! {joke.setup} {joke.punchline}");
+
+            }
+        }
     }
-  }
 }
