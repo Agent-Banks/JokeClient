0a8ebf03431e5bc932ae963962ed3268cdbdda76
CodyBanks <codydouglasbanks@gmail.com>
2020-06-02 16:41:56 -0400

    Added two features for general jokes

--- Program.cs
+++ Program.cs
@@ -9,21 +9,6 @@ namespace JokeClient
 {
     class Program
     {
-        class Joke
-        {
-            [JsonPropertyName("id")]
-            public int Id { get; set; }
-
-            [JsonPropertyName("type")]
-            public string Type { get; set; }
-
-            [JsonPropertyName("setup")]
-            public string Setup { get; set; }
-
-            [JsonPropertyName("punchline")]
-            public string Punchline { get; set; }
-        }
-
         static int PromptForInteger(string prompt)
         {
             Console.Write(prompt);
@@ -43,12 +28,15 @@ namespace JokeClient
         }
         static async Task Main(string[] args)
         {
-            var userWantsToQuit = false;
-            while (userWantsToQuit == false)
+            var userWantsToQuit = true;
+            while (userWantsToQuit)
             {
                 Console.WriteLine("(1)- View one random programming joke");
-                Console.WriteLine("(2)- View 10 random programing jokes");
-                Console.WriteLine("(3)- Quit the application");
+                Console.WriteLine("(2)- View ten random programing jokes");
+                Console.WriteLine("(3)- View one general joke");
+                Console.WriteLine("(4)- View ten random general jokes");
+                Console.WriteLine("(5)- Quit the application");
+
                 var option = PromptForInteger("Option: ");
 
                 if (option == 1)
@@ -85,7 +73,39 @@ namespace JokeClient
 
                 if (option == 3)
                 {
-                    userWantsToQuit = true;
+                    var client = new HttpClient();
+
+                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/general/random");
+                    var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+
+                    foreach (var joke in jokes)
+                    {
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine($"This is a {joke.Type} joke! {joke.Setup} {joke.Punchline}");
+                        Console.WriteLine("----------------------------");
+
+                    }
+                }
+
+                if (option == 4)
+                {
+                    var client = new HttpClient();
+
+                    var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/general/ten");
+                    var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+
+                    foreach (var joke in jokes)
+                    {
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine($"This is a {joke.Type} joke! {joke.Setup} {joke.Punchline}");
+                        Console.WriteLine("----------------------------");
+
+                    }
+                }
+
+                if (option == 5)
+                {
+                    userWantsToQuit = false;
                 }
             }
         }
